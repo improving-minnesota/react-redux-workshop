34357647ba81f5cba422481da4bb59f1009eac73
Dustin Schau <dustinschau@gmail.com>
2018-04-27 16:37:19 -0500

    chore: minor fixes

--- slidedeck/webpack.config.development.js
+++ slidedeck/webpack.config.development.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 module.exports = function webpackDevConfig(baseConfig) {
-  baseConfig.mode = 'development';
-
   return Object.assign({}, baseConfig, {
     mode: 'development',
     devServer: {
