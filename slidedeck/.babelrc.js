3ae6cdeed031f09bed5fa3775343f288700b9d5b
Dustin Schau <dustinschau@gmail.com>
2018-04-30 09:19:43 -0500

    build: actually add browserslist

--- slidedeck/.babelrc.js
+++ slidedeck/.babelrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   presets: [
-    ['babel-preset-env'],
+    [
+      'babel-preset-env',
+      {
+        targets: {
+          browsers: ['>1%', 'not ie 11', 'not op_mini']
+        }
+      }
+    ],
     'babel-preset-react'
   ]
 };
