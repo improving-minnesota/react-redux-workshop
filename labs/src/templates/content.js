9cdeae544380f8a889e714a552a53af21733d8b6
Dustin Schau <dustinschau@gmail.com>
2018-05-17 13:54:44 -0500

    feat: add link to next lab

--- labs/src/templates/content.js
+++ labs/src/templates/content.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'react-emotion';
 import 'prismjs/themes/prism-okaidia.css';
+import GatsbyLink from 'gatsby-link';
+import ChevronRight from 'react-icons/lib/fa/chevron-right';
+
+import { Button } from '@objectpartners/components';
 
 const Container = styled.div({
   fontFamily: 'sans-serif',
@@ -10,11 +14,43 @@ const Container = styled.div({
   },
 });
 
-export default function Content({ data }) {
+const Link = styled(Button)({
+  display: 'block',
+  margin: '1rem auto',
+  position: 'relative',
+  textAlign: 'center',
+  textDecoration: 'none',
+  width: '100%',
+  ':hover': {
+    svg: {
+      color: '#1a1a1a',
+    },
+  },
+  '@media only screen and (min-width: 768px)': {
+    maxWidth: '50%',
+  },
+}).withComponent(GatsbyLink);
+
+const NextLabIcon = styled(ChevronRight)({
+  color: 'white',
+  position: 'absolute',
+  right: 8,
+});
+
+export default function Content({ data, pathContext }) {
   const { content } = data;
+  const { next } = pathContext;
+
+  const isLab = content.fields.type === 'lab';
   return (
     <Container>
       <div dangerouslySetInnerHTML={{ __html: content.html }} />
+      {isLab && next.node ? (
+        <Link to={next.node.fields.slug}>
+          {next.node.frontmatter.title}
+          <NextLabIcon />
+        </Link>
+      ) : null}
     </Container>
   );
 }
@@ -23,15 +59,17 @@ export const pageQuery = graphql`
   fragment ContentFragment on MarkdownRemark {
     fields {
       slug
+      type
     }
     frontmatter {
       title
     }
+    html
   }
 
-  query LabQuery($slug: String!) {
+  query ContentQuery($slug: String!) {
     content: markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
+      ...ContentFragment
     }
   }
 `;
