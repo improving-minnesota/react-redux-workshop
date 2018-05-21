ec08f5f4f11df9846dc471fafac5d1279d5306bb
Dustin Schau <dustinschau@gmail.com>
2018-05-21 15:51:50 -0500

    chore: tweak slide section name; add special topics

---
+++ slidedeck/src/components/react-logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from './react.svg';
+
+import './react-logo.css';
+
+export default function ReactLogo() {
+  return <img className="react-logo" src={logo} />;
+}
---
+++ slidedeck/src/slides/08-special-topics/01-react-16-intro.md
@@ -0,0 +1,5 @@
+import Logo from '../../components/react-logo';
+
+## React 16.3
+
+<Logo />
