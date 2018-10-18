849645409bc6f170c31b801f6d77b8206572143d
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-10-17 19:43:18 -0500

    Upgrading slidedeck libraries

--- slidedeck/.babelrc.js
+++ slidedeck/.babelrc.js
@@ -1,14 +1,25 @@
 module.exports = {
   presets: [
     [
-      'babel-preset-env',
+      '@babel/preset-env',
       {
         targets: {
           browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
         }
       }
     ],
-    'babel-preset-stage-2',
-    'babel-preset-react'
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    [
+      'prismjs',
+      {
+        languages: ['groovy', 'json', 'jsx', 'flow', 'typescript'],
+        plugins: ['keep-markup', 'unescaped-markup', 'normalize-whitespace'],
+        theme: 'okaidia',
+        css: true
+      }
+    ]
   ]
 };
